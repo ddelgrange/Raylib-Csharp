10a1b77ec07441bbf926396833bf219b7e091e55
David Delgrange <daviddelgrange@yahoo.fr>
2024-07-25 21:08:22 +0200

    ghj

--- 1_codeMinimum/obj/Debug/net8.0/1_codeMinimum.AssemblyInfo.cs
+++ 1_codeMinimum/obj/Debug/net8.0/1_codeMinimum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1_codeMinimum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e3a559a50422a037f929bb84063909664ca4fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("1_codeMinimum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1_codeMinimum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
